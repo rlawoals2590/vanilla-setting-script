8a852db0f9760f28b5ca2f5bfcd85d41fc2dfd5f
rlawoals2590 <tuiab25906370@gmail.com>
2023-03-12 09:17:48 +0900

    add : select aws use region

--- awscli-config.ps1
+++ awscli-config.ps1
@@ -1,5 +1,6 @@
 $access_key = Read-Host "Input Youre AWS Access Key "
 $secret_key = Read-Host "Input Youre AWS Secret Key "
+$region = Read-Host "Input Youre Use AWS Region "
 
 $filePath = "C:\Program Files\Amazon\AWSCLIV2\config.ps1"
 $awsPath = "C:\Program Files\Amazon\AWSCLIV2\aws"
@@ -8,9 +9,10 @@ New-Item -ItemType File -Path $filePath -Force
 Add-Content -Path $filePath -Value "# AWS Account Login"
 Add-Content -Path $filePath -Value "& '$awsPath' configure set aws_access_key_id your_access_key"
 Add-Content -Path $filePath -Value "& '$awsPath' configure set aws_secret_access_key your_secret_key"
-Add-Content -Path $filePath -Value "& '$awsPath' configure set region ap-northeast-2"
+Add-Content -Path $filePath -Value "& '$awsPath' configure set region us-west-2"
 
 (Get-Content $filePath) | Foreach-Object { $_ -replace "your_access_key", "$access_key" } | Out-File $filePath
 (Get-Content $filePath) | Foreach-Object { $_ -replace "your_secret_key", "$secret_key" } | Out-File $filePath
+(Get-Content $filePath) | Foreach-Object { $_ -replace "us-west-2", "$region" } | Out-File $filePath
 
 & 'C:\Program Files\Amazon\AWSCLIV2\config.ps1'
